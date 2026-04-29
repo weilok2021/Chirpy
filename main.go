227f75aa8bfd99c21ccaafa3643c8952cf12bace
weilok2021 <weilok629@gmail.com>
2026-04-29 21:42:37 +0800

    feat: add chirp validation endpoint and move metrics to /admin

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -29,8 +30,10 @@ func main() {
 	mux.Handle("/app/logo.png", (&cfg).middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("./assets")))))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /api/metrics", (&cfg).handlerCountRequests)
-	mux.HandleFunc("POST /api/reset", (&cfg).handlerResetRequestCount)
+	mux.HandleFunc("POST /api/validate_chirp", handlerFirstJson)
+	mux.HandleFunc("GET /admin/metrics", (&cfg).handlerCountRequests)
+	mux.HandleFunc("POST /admin/reset", (&cfg).handlerResetRequestCount)
+
 	log.Fatal(server.ListenAndServe())
 }
 
@@ -43,6 +46,68 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func handlerFirstJson(w http.ResponseWriter, r *http.Request) {
+	type requestJson struct {
+		Body string `json:"body"`
+	}
+	type errorJson struct {
+		Error string `json:"error"`
+	}
+	type returnJson struct {
+		Valid bool `json:"valid"`
+	}
+
+	// this method will returns json either normal, or error json
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	req := requestJson{}
+	err := decoder.Decode(&req)
+	if err != nil {
+		resp := errorJson{
+			Error: "Something went wrong",
+		}
+		dat, err := json.Marshal(resp)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		w.Write(dat)
+		return
+	}
+
+	if len(req.Body) > 140 {
+		resp := errorJson{
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(resp)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+
+	resp := returnJson{
+		Valid: true,
+	}
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
+	w.Write(dat)
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
@@ -52,7 +117,10 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerCountRequests(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte(fmt.Sprintf("Hits: %v\n", cfg.fileserverHits.Load()))); err != nil {
+	// Set response content type to text/html
+	w.Header().Add("Content-Type", "text/html")
+	if _, err := w.Write([]byte(fmt.Sprintf(`<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>
+`, cfg.fileserverHits.Load()))); err != nil {
 		log.Fatal(err)
 	}
 }
