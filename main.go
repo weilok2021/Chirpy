0a24ae1784e9742a20ba224184cad19889f30364
weilok <weilok.chia@redtone.com>
2026-04-29 16:25:23 +0800

    feat: add metrics middleware and API endpoints
    Track fileserver hits via atomic counter, expose /api/metrics and
    /api/reset, and namespace endpoints under /api.

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -17,16 +23,41 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.Handle("/app/logo.png", http.StripPrefix("/app/", http.FileServer(http.Dir("./assets"))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	cfg := apiConfig{fileserverHits: atomic.Int32{}}
+
+	mux.Handle("/app/", (&cfg).middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.Handle("/app/logo.png", (&cfg).middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("./assets")))))
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", (&cfg).handlerCountRequests)
+	mux.HandleFunc("POST /api/reset", (&cfg).handlerResetRequestCount)
 	log.Fatal(server.ListenAndServe())
+}
+
+// middleware method that wrap handler function to increment fileserverHits
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("middleware hit: %s", r.URL.Path)
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	if _, err := w.Write([]byte("OK")); err != nil {
-		errors.New("Error writing into response body!")
+	if _, err := w.Write([]byte("OK\n")); err != nil {
+		log.Fatal(errors.New("Error writing into response body!"))
 	}
+}
+
+func (cfg *apiConfig) handlerCountRequests(w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte(fmt.Sprintf("Hits: %v\n", cfg.fileserverHits.Load()))); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (cfg *apiConfig) handlerResetRequestCount(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
 }
